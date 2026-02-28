a1176fd915be10749d6d43028a7709961e1e732a
JesssseL <146840057+JesssseL@users.noreply.github.com>
2026-02-28 16:53:38 +0000

    Add movement

--- gatwick-go/src/views/AboutView.vue
+++ gatwick-go/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div ref="canvasContainer"></div>
+  <div ref="canvasContainer">
+    <button class="button button-left" @click="goLeft"> < </button>
+    <button class="button button-right" @click="goRight"> > </button>
+  </div>
 </template>
 
 <script>
@@ -7,7 +10,8 @@ export default {
   name: "P5Canvas",
   data() {
     return {
-      p5Instance: null
+      p5Instance: null,
+      xAxis: 50
     };
   },
   mounted() {
@@ -20,10 +24,18 @@ export default {
       p.draw = () => {
         //many plane things could go here
         p.fill(255, 0, 0);
-        p.ellipse(p.width / 2, p.height / 2, 50, 50);
+        p.ellipse(p.width * (this.xAxis / 100), p.height / 2, 50, 50);
       };
     };
     this.p5Instance = new window.p5(sketch, this.$refs.canvasContainer);
+  },
+  methods: {
+    goLeft() {
+      this.xAxis--
+    },
+    goRight() {
+      this.xAxis++
+    }
   },
   beforeUnmount() {
     if (this.p5Instance) {
@@ -42,5 +54,22 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
+}
+
+.button {
+  position: absolute;
+  bottom: 24px;
+  height: 25%;
+  background: red;
+  border: none;
+  padding: 36px;
+}
+
+.button-left {
+  left: 24px; 
+}
+
+.button-right {
+  right: 24px; 
 }
 </style>
